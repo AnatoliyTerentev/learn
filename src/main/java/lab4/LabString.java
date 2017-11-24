8bffd7edf56a22a2acfeb24aca9544ec9272c954
Anatoliy_Terentev <ant_x@list.ru>
2017-11-24 16:40:10 +0400

    Workstation configuration

---
+++ src/main/java/lab4/LabString.java
@@ -0,0 +1,12 @@
+package lab4;
+
+public class LabString {
+    private static Object str = "Java - - строго типизированный объектно-ориентированный язык программирования, " +
+            "разработанный компанией Sun Microsystems в последующем приобретённой компанией Oracle";
+
+
+    public static void main(String[] args) {
+        /*1.Получение длины строки*/
+        System.out.println(str);
+    }
+}
